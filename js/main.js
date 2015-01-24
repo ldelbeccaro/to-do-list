353c42741094726244ab3a7427d35e72df91a4df
Laura Del Beccaro <lauradelbeccaro@gmail.com>
2015-01-24 02:05:55 -0500

    Fixed show/hide archived tasks bug

--- js/main.js
+++ js/main.js
@@ -4,9 +4,6 @@ $(document).ready(function () {
 	var restore = '<img class="restore" src="images/restore.svg" alt="restore" style="display: none">';
 	var del = '<img class="delete" src="images/delete.svg" alt="delete" style="display: none">';
 	var archive = '<img class="archive" src="images/archive.svg" alt="archive" style="display: none">'
-	var tasks = 0;
-	var complete = 0;
-	var hidden = 0;
 
 	/* Create new task from text input */
 	$( 'form' ).submit(function ( event ) {
@@ -20,8 +17,6 @@ $(document).ready(function () {
 		// still need to animate this????
 		event.preventDefault();
 		$( 'input:first' ).val('');
-		tasks ++;
-		console.log('Tasks: ' + tasks + '\n Complete: ' + complete + '\n Complete & Hidden: ' + hidden);
 	});
 
 	/* Show delete button on task hover */
@@ -41,13 +36,13 @@ $(document).ready(function () {
 			$( this )
 				.addClass('hover')
 				.find('img').show()
-				.find('p').css('margin-left', '5px');
+				.find('p').css('margin-left', '-=30');
 		})
 		.on('mouseleave', 'li', function ( event ) {
 			$( this )
 				.removeClass('hover')
 				.find('img').hide()
-				.find('p').css('margin-left', '35px');
+				.find('p').css('margin-left', '+=30');
 		});
 
 	/* Check off task */
@@ -57,9 +52,6 @@ $(document).ready(function () {
 		var completeTask = '<li class="complete">' + restore + taskText + archive + '</li>';
 		$( 'ul.complete' ).append(completeTask);
 		$( this ).parent().remove();
-		tasks --;
-		complete ++;
-		console.log('Tasks: ' + tasks + '\n Complete: ' + complete + '\n Complete & Hidden: ' + hidden);
 	});
 
 	/* Restore task */
@@ -69,29 +61,21 @@ $(document).ready(function () {
 		var incompleteTask = '<li class="task">' + check + taskText + del + '</li>';
 		$( 'ul.to-do' ).append(incompleteTask);
 		$( this ).parent().remove();
-		tasks ++;
-		complete --;
-		console.log('Tasks: ' + tasks + '\n Complete: ' + complete + '\n Complete & Hidden: ' + hidden);
 	});
 
 	/* Delete task */
 	$( '.list ul' ).on('click', 'li > img.delete', function ( event ) {
 		console.log('Task deleted');
 		$( this ).parent().remove();
-		tasks --;
-		console.log('Tasks: ' + tasks + '\n Complete: ' + complete + '\n Complete & Hidden: ' + hidden);
 	});
 
-	/* Hide complete task */
+	/* Archive complete task */
 	$( '.list ul' ).on('click', 'li > img.archive', function ( event ) {
 		console.log('Complete task hidden');
 		var taskText = '<p>' + $( this ).parent().text() + '</p>';
 		var hiddenTask = '<li class="complete">' + restore + taskText + '</li>';
 		$( 'ul.hidden' ).append(hiddenTask);
 		$( this ).parent().remove();
-		hidden ++;
-		complete --;
-		console.log('Tasks: ' + tasks + '\n Complete: ' + complete + '\n Complete & Hidden: ' + hidden);
 	});
 
 	/* Archive all completed tasks */
@@ -100,27 +84,23 @@ $(document).ready(function () {
 		$( 'ul.complete' ).find( 'img.archive' ).remove();
 		$( 'ul.hidden' ).append( $( 'ul.complete' ).html() );
 		$( 'ul.complete' ).children().remove();
-		hidden += $( 'ul.hidden' ).length();
-		complete = 0;
-		console.log('Tasks: ' + tasks + '\n Complete: ' + complete + '\n Complete & Hidden: ' + hidden);
 	});
 
-	/* Show hidden tasks */
-	$( '.unhide' ).click( function ( event ) {
-		$( 'ul.hidden' ).show();
-		$( this )
-			.text( 'Hide archived' )
-			.addClass( 'hide-complete' )
-			.removeClass( 'unhide' );
-	});
-
-	/* Hide hidden tasks */
-	$( '.hide-complete' ).click( function ( event ) {
-		$( 'ul.hidden' ).hide();
-		$( this )
-			.text( 'Show archived' )
-			.addClass( 'unhide' )
-			.removeClass( 'hide-complete' );
+	/* Show or hide archived tasks */
+	$( function () {
+		$( '.unhide' ).click (function ( event ) {
+//			$( '.unhide' ).slideToggle();
+			if ( $( '.unhide' ).text() === 'Show archived' ) {
+				$( 'ul.hidden' ).show();
+				$( this )
+					.text( 'Hide archived' );
+			}
+			else {
+				$( 'ul.hidden' ).hide();
+				$( this )
+					.text( 'Show archived' );
+			}
+		});
 	});
 
 	/* Delete all tasks */
@@ -128,9 +108,6 @@ $(document).ready(function () {
 		$( 'ul.to-do' ).children().remove();
 		$( 'ul.complete' ).children().remove();
 		$( 'ul.hidden' ).children().remove();
-		tasks = 0;
-		complete = 0;
-		hidden = 0;
 	});
 
 	/* Reorder tasks */
